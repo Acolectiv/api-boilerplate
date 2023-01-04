2f44110471f995c34e650bb7dfc2b4fe847acf66
Alexa Matei <alexa.matei.dev@icloud.com>
2023-01-04 18:08:30 +0200

    limiter

--- src/main.js
+++ src/main.js
@@ -8,9 +8,16 @@ const Logger = require("./utils/logger");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const store = require("./managers/MemoryStore");
+const rateLimit = require("express-rate-limit");
+
+let limiter = new rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10
+});
 
 const app = express();
 
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
